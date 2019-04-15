9c917b77707282b84c0e832119a7892b78c16cff
Marvin Zachary <acerzach@gmail.com>
2019-04-15 12:45:46 +0300

    Added import classes to app.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,11 @@
-// import java.util.Map;
-// import java.util.HashMap;
-// import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
 
-// import spark.ModelAndView;
-// import spark.template.velocity.VelocityTemplateEngine;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
 
-// import static spark.Spark.*;
+import static spark.Spark.*;
 
 // public class App {
 //   public static void main(String[] args) {
