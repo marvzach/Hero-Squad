34aa2ce2f0f6481cb140f222187c89b89101fc15
Marvin Zachary <acerzach@gmail.com>
2019-04-15 13:06:59 +0300

    Added routing to find squad

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -65,13 +65,13 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-//     get("/squads/:id", (request, response) -> {
-//       Map<String, Object> model = new HashMap<String, Object>();
-//       Squad squad = Squad.find(Integer.parseInt(request.params(":id")));
-//       model.put("squad", squad);
-//       model.put("template", "templates/squad.vtl");
-//       return new ModelAndView(model, layout);
-//     }, new VelocityTemplateEngine());
+    get("/squads/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Squad squad = Squad.find(Integer.parseInt(request.params(":id")));
+      model.put("squad", squad);
+      model.put("template", "templates/squad.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
 //     get("squads/:id/heroes/new", (request, response) -> {
 //       Map<String, Object> model = new HashMap<String, Object>();
